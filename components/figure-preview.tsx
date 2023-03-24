ea1c6bde9a2727e907225373ce71865eed88a323
mattmolinare <mattmolinare@gmail.com>
2023-03-24 11:08:48 -0800

    fix: conditionally render figure preview

--- components/figure-preview.tsx
+++ components/figure-preview.tsx
@@ -4,6 +4,10 @@ import { getUrl } from "../lib/s3";
 const FigurePreview = (props: PreviewProps) => {
   const { s3Key, alt, caption } = props as any;
 
+  if (s3Key === undefined) {
+    return null;
+  }
+
   return (
     <figure>
       <img src={getUrl(s3Key)} alt={alt} width="100%" />
