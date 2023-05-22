645164229198cbcfbaffb7fe79d8e92263495c08
mattmolinare@gmail.com <mattmolinare@gmail.com>
2023-05-22 16:58:25 -0700

    chore: disable google maps input
    Plugin does not allow user to provide exact latitude/longitude
    coordinates. Disable for now.

--- sanity.config.ts
+++ sanity.config.ts
@@ -21,11 +21,11 @@ export default defineConfig({
   plugins: [
     deskTool(),
     visionTool(),
-    googleMapsInput({
-      apiKey: import.meta.env.SANITY_STUDIO_GOOGLE_MAPS_API_KEY,
-      defaultZoom: 8,
-      defaultLocation: { lat: 34.289129, lng: -117.646304 },
-    }),
+    // googleMapsInput({
+    //   apiKey: import.meta.env.SANITY_STUDIO_GOOGLE_MAPS_API_KEY,
+    //   defaultZoom: 8,
+    //   defaultLocation: { lat: 34.289129, lng: -117.646304 },
+    // }),
   ],
   schema: {
     types: [
