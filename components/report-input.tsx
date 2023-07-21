f4aa0f40bb12b8260e58d193f4a707df6d86a49b
mattmolinare <mattmolinare@gmail.com>
2023-07-21 15:44:23 -0700

    feat: update report input component
    Use listening query to fetch photo count.
    
    Provide feedback when figures are added.
    
    Position input/button below markdown editor.
    
    Use correct properties type.

--- components/report-input.tsx
+++ components/report-input.tsx
@@ -1,14 +1,17 @@
 import { AddIcon } from "@sanity/icons";
-import { Box, Button, Flex, Stack, TextInput } from "@sanity/ui";
+import { Box, Button, Flex, Stack, TextInput, useToast } from "@sanity/ui";
 import { randomKey } from "@sanity/util/content";
 import endent from "endent";
-import { useCallback, useEffect, useState } from "react";
-import type { ArrayOfPrimitivesInputProps } from "sanity";
+import { useCallback, useState } from "react";
+import type { ArrayOfObjectsInputProps } from "sanity";
 import { insert, setIfMissing } from "sanity";
+import { useListeningQuery } from "sanity-plugin-utils";
 import useDebouncedValue from "../hooks/debounced-value";
 import useSanityClient from "../hooks/sanity-client";
 
-const ReportInput = (props: ArrayOfPrimitivesInputProps) => {
+const ReportInput = (props: ArrayOfObjectsInputProps) => {
+  const toast = useToast();
+
   const client = useSanityClient();
 
   const [inputValue, setInputValue] = useState("");
@@ -22,22 +25,54 @@ const ReportInput = (props: ArrayOfPrimitivesInputProps) => {
     ]
   `;
 
-  const [count, setCount] = useState<number>();
+  const { data: count } = useListeningQuery<number>(
+    endent`
+      count(
+        ${query}
+      )
+    `,
+    {}
+  );
 
-  useEffect(() => {
-    (async () => {
-      const count = await client.fetch<number>(endent`
-        count(
-          ${query}
-        )
-      `);
+  const handleClick = useCallback(async () => {
+    const ids = await client.fetch<string[]>(endent`
+      ${query}
+      | order(s3Key asc)
+      ._id
+    `);
 
-      setCount(count);
-    })();
+    if (ids.length === 0) {
+      return;
+    }
+
+    props.onChange([
+      setIfMissing([]),
+      insert(
+        ids.map((id) => ({
+          _key: randomKey(12),
+          _type: "figure",
+          alt: "",
+          photo: {
+            _ref: id,
+            _type: "reference",
+            _weak: true,
+          },
+        })),
+        "after",
+        [-1]
+      ),
+    ]);
+
+    toast.push({
+      status: "success",
+      title: `${ids.length} figure${ids.length === 1 ? "" : "s"} added`,
+      closable: true,
+    });
   }, [query]);
 
   return (
     <Stack space={3}>
+      {props.renderDefault(props)}
       <Flex gap={1}>
         <Box flex={1}>
           <TextInput
@@ -49,43 +84,19 @@ const ReportInput = (props: ArrayOfPrimitivesInputProps) => {
           />
         </Box>
         <Button
-          icon={AddIcon}
+          disabled={count === null || count === 0}
+          icon={count !== null && count > 0 ? AddIcon : undefined}
           text={
-            count === undefined
+            count === null
               ? "Add figures"
               : count === 0
               ? "No figures found"
               : `Add ${count} figure${count === 1 ? "" : "s"}`
           }
           mode="ghost"
-          onClick={useCallback(async () => {
-            const ids = await client.fetch<string[]>(endent`
-              ${query}
-              | order(s3Key asc)
-              ._id
-            `);
-
-            props.onChange([
-              setIfMissing([]),
-              insert(
-                ids.map((id) => ({
-                  _key: randomKey(12),
-                  _type: "figure",
-                  alt: "",
-                  photo: {
-                    _ref: id,
-                    _type: "reference",
-                    _weak: true,
-                  },
-                })),
-                "after",
-                [-1]
-              ),
-            ]);
-          }, [query])}
+          onClick={handleClick}
         />
       </Flex>
-      {props.renderDefault(props)}
     </Stack>
   );
 };
