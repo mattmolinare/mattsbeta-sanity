cf984b81826e93786da56d37c267e5228750eba2
mattmolinare <mattmolinare@gmail.com>
2024-05-28 14:49:03 -0700

    chore: update git hooks

--- commitlint.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "header-max-length": [2, "always", 50],
-    "body-max-line-length": [2, "always", 72],
-    "footer-max-line-length": [2, "always", 72],
-  },
-};
